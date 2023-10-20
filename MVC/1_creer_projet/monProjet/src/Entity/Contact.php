1e5553567fd53e9e585f881c8ac590f1987d53b0
AMMOUCHI23 <bily-f@hotmail.fr>
2023-10-20 11:08:17 +0200

    fin de chapitre formulaire de symfony

--- MVC/1_creer_projet/monProjet/src/Entity/Contact.php
+++ MVC/1_creer_projet/monProjet/src/Entity/Contact.php
@@ -6,6 +6,8 @@ use App\Repository\ContactRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
+
+
 #[ORM\Entity(repositoryClass: ContactRepository::class)]
 class Contact
 {
