b4e691abc1c52e7058d233407429be28564a5403
AMMOUCHI23 <bily-f@hotmail.fr>
2023-12-08 10:03:19 +0100

    dernier commit de fin de formation

--- mon-projet-react/src/components/Categorie.jsx
+++ mon-projet-react/src/components/Categorie.jsx
@@ -1,11 +1,39 @@
 import axios from 'axios'
+import React, { useState, useEffect } from 'react';
 
 
 const Categorie = () => {
-    axios.get('https://127.0.0.1:8000/api/categories?page=')
+    const [categories, setCategories]=useState([]);
+    
+    // on utilise useEffect pour effectuer la requete dés le rendu initial
+     useEffect(() => {
+        axios.get('https://127.0.0.1:8000/api/categories?page=1')
         .then((response) => {
-            console.log(response.data['hydra:member']);
+            setCategories(response.data['hydra:member']);
         });
+
+    }, [] );
+    
+        return(
+            <div class="container">
+            <h1>Liste des Categorie</h1>
+            <div class="row">
+                {
+                categories.map((category) =>(
+                <div  key={category['@id']} className='col-3'> <img
+                src={`assets/img/category/${category.image}`}
+               
+                alt={category.libelle}
+                thumbnail
+                width={400}
+                height={400}
+              />
+              </div>
+                ))
+                }
+            </div>
+            </div>
+        )
 };
 
 export default Categorie;
--- the_district/react/my_react_app/jsx/Categories.jsx
+++ the_district/react/my_react_app/jsx/Categories.jsx
@@ -1,3 +1,51 @@
+import axios from 'axios'
+import '/assets/styles/app.css';
+import React, { useState, useEffect } from 'react';
+
+
+const Categorie = () => {
+    const [categories, setCategories]=useState([]);
+    
+    // on utilise useEffect pour effectuer la requete dés le rendu initial
+     useEffect(() => {
+        axios.get('https://127.0.0.1:8000/api/categories?page=1')
+        .then((response) => {
+            setCategories(response.data['hydra:member']);
+        })
+        .catch((error) => {
+          console.error("Une erreur s'est produite lors de la récupération des catégories :", error);
+      });
+
+    }, [] );
+    
+        return(
+            <div className="container text-center text-primary my-4">
+             <h1 className="my-4">Nos Catégories des plats</h1>
+            <div className="row justify-content-center">
+                {
+                categories.map((category) =>(
+                <div  key={category['@id']} className=" col-sm-5 col-md-4 my-4  "> 
+                <a href="">
+                <img 
+                src={`assets/img/category/${category.image}`}
+               
+                alt={category.libelle}
+                thumbnail
+                width={350}
+                
+              />
+              </a>
+               <h3 className="nomCategorie my-2">{category.libelle}</h3>
+              </div>
+                ))
+                }
+            </div>
+            </div>
+        )
+};
+
+export default Categorie;
+/*
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import '/assets/styles/app.css';
@@ -44,4 +92,4 @@ const Categories = () => {
   );
 };
 
-export default Categories;
+export default Categories;*/
